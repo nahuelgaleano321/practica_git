51f47c52c3d7ed6edc57fb3725a79a0ca1193af6
Lucas Rimbano <lucasrimbano@gmail.com>
2026-04-08 00:41:47 -0300

    Agregando lista

--- hola.py
+++ hola.py
@@ -3,3 +3,15 @@ print("hola mundo,soy nahuel")
 a = 5 
 
 print(a + 10)
+
+titulo = "que haces"
+
+nombre = "nahuel"
+
+print(titulo + ":" + nombre + " !!!")
+
+
+lista = ["nahuel" , "lucas" ,"maria" ,"juansito"]
+
+for elemento in lista:
+    print(elemento)
